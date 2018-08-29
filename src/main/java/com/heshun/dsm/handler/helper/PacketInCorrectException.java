04efd8d0bc217b360f8e67f1c6833b23d3b6fb2f
PC-201307182222\Administrator <sun_flo@yeah.net>
2018-08-29 22:16:17 +0800

    新增构造方法，用于中电管理机乱码情况下

--- src/main/java/com/heshun/dsm/handler/helper/PacketInCorrectException.java
+++ src/main/java/com/heshun/dsm/handler/helper/PacketInCorrectException.java
@@ -4,7 +4,7 @@ package com.heshun.dsm.handler.helper;
  * 针对仪表数据没有正确采集，全是0，抛出的异常
  * 
  * @author huangxz
- *
+ * 
  */
 public class PacketInCorrectException extends Exception {
 
@@ -12,5 +12,9 @@ public class PacketInCorrectException extends Exception {
 
 	public PacketInCorrectException() {
 		super("仪表数据采集异常");
+	}
+
+	public PacketInCorrectException(Exception e) {
+		super("数据异常，可能管理机报文存在错误或其他原因，忽略掉所有未处理报文", e);
 	}
 }
